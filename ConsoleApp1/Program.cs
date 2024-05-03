48a78458b74d5464a1564b08a88e63f80242ec05
asndp <asndp@SANDIP>
2024-05-03 12:11:30 +0545

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class MyHashSet
+{
+    static int size = 1000;
+    List<int>[] bucket = new List<int>[size];
+
+    public MyHashSet()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            bucket[i] = new List<int>();
+        }
+
+    }
+    public int gethash(int key)
+    {
+        int a = key % size;
+        return a;
+    }
+
+    public void Add(int key)
+    {
+      int  index = gethash(key);
+        if (!bucket[index].Contains(key)) 
+        { 
+            bucket[index].Add(key); 
+        }
+    }
+
+    public void Remove(int key)
+    {
+        int index1 = gethash(key);
+        bucket[index1].Remove(key);
+    }
+
+    public bool Contains(int key)
+    {
+        int index1 = gethash(key);
+        return bucket[index1].Contains(key);
+        
+
+    }
+}
+public class MyHashMap
+{
+
+    public MyHashMap()
+    {
+
+    }
+
+    public void Put(int key, int value)
+    {
+
+    }
+
+    public int Get(int key)
+    {
+
+    }
+
+    public void Remove(int key)
+    {
+
+    }
+}
